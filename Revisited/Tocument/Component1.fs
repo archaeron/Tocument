9e25feb675d262add9023486ea69a405eca0a301
Joris Morger <jorismorger@gmail.com>
2014-04-02 20:34:43 +0200

    refactored

--- Revisited/Tocument/Component1.fs
+++ Revisited/Tocument/Component1.fs
@@ -6,20 +6,27 @@
 module Sqlite
 
 module Reader =
-    open Mono.Data.Sqlite
-    open System.Data
-    let createConnection (path:string) = new SqliteConnection ("Data Source=" + path + ",version=3")
-    let connection = createConnection "/home/jorismorger/Projects/Tocument/BackboneJS.docset/Contents/Resources/docSet.dsidx"
-    connection.Open()
-    let query txt args extractor =
-        let cmd = connection.CreateCommand()
-        cmd.CommandText <- txt
-        for a,v in args do
-            cmd.Parameters.AddWithValue(a,v) |> ignore
-        seq {
-            use r = cmd.ExecuteReader()
-            while(r.Read()) do
-                yield (extractor r)
-        }
-  
+    module Query = 
+        open Mono.Data.Sqlite
+        open System.Data
+        let createConnection (path:string) = new SqliteConnection ("Data Source=" + path + ",version=3")
+        let connection = createConnection "/home/jorismorger/Projects/Tocument2/Revisited/BackboneJS.docset/Contents/Resources/docSet.dsidx"
+        connection.Open()
+        let search txt args extractor =
+            let cmd = connection.CreateCommand()
+            cmd.CommandText <- txt
+            for a,v in args do
+                cmd.Parameters.AddWithValue(a,v) |> ignore
+            seq {
+                use r = cmd.ExecuteReader()
+                while(r.Read()) do
+                    yield (extractor r)
+            }
+      
+    
+    let indexAll extractor = 
+        Query.search "SELECT name, path, type FROM searchIndex ORDER BY name LIMIT 100" [] extractor
+    
+    let indexWhere where extractor = 
+        Query.search ("SELECT name, path, type FROM searchIndex WHERE " + where + " ORDER BY name LIMIT 100") [] extractor
 
--- Revisited/TocumentGTK/Main.fs
+++ Revisited/TocumentGTK/Main.fs
@@ -10,10 +10,17 @@
         let Main(args) = 
             Application.Init()
             let win = new MainWindow.MyWindow()
-            for j,v,t in Sqlite.Reader.query  "SELECT name, path, type FROM searchIndex ORDER BY name LIMIT 100" [] (fun r -> (r.GetValue 0,r.GetValue 1,r.GetValue 2)) do
+            //for j,v,t in Sqlite.Reader.indexAll (fun r -> (r.GetValue 0,r.GetValue 1,r.GetValue 2)) do
+              //  printfn "Method %A" j
+                //printfn "Path %A" v
+                //printfn "Type %A" t
+
+
+            for j,v,t in Sqlite.Reader.indexWhere "name = 'View.render'" (fun r -> (r.GetValue 0,r.GetValue 1,r.GetValue 2)) do
                 printfn "Method %A" j
                 printfn "Path %A" v
                 printfn "Type %A" t
+
 
             win.Show()
             Application.Run()
