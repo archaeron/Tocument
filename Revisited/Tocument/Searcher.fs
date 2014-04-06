29803af7cf172938b9672982332b53d82d501f2c
Nicolas Gagliani <whatamaps@gmail.com>
2014-04-06 17:43:08 +0200

    search LINQ-Style

--- Revisited/Tocument/Searcher.fs
+++ Revisited/Tocument/Searcher.fs
@@ -8,75 +8,67 @@ http://code.google.com/p/dblinq2007/downloads/list
 For this example, an SQLite version of the Northwind Database was used; this file is included in the System.Data.SQLite download
 *)
 
-let pathToSQLiteDB = @"C:\Users\Nicolas\Documents\GitHub\Tocument\Revisited\BackboneJS.docset\Contents\Resources\docSet.dsidx"
-
+let pathToSQLiteDB = 
+    @"C:\Users\Nicolas\Documents\GitHub\Tocument\Revisited\BackboneJS.docset\Contents\Resources\docSet.dsidx"
 
 //#r @"D:\Projects\Research\Libraries\FSharp.PowerPack.dll"
 //#r @"D:\Projects\Research\Libraries\FSharp.PowerPack.Linq.dll"
-
 //#r @"D:\Projects\Research\Libraries\DbLinq.dll"
 //#r @"D:\Projects\Research\Libraries\DbLinq.SQLite.dll"
 //#r @"D:\Projects\Research\Libraries\System.Data.SQLite.dll"
-
 open System.Data.Linq
 open System.Data.Linq.Mapping
 open System.Data.SQLite
-
 open Microsoft.FSharp.Linq
 
 //open Microsoft.FSharp.Linq.Query
-
-[<Table(Name="searchIndex")>]
-type SearchIndex (customerId:string,companyName:string,contactName:string)=
-    let mutable m_customerId = customerId
-    let mutable m_companyName = companyName
-    let mutable m_contactName = contactName
-
+[<Table(Name = "searchIndex")>]
+type SearchIndex(name : string, path : string, docType : string) = 
+    let mutable m_name = name
+    let mutable m_path = path
+    let mutable m_docType = docType
     new() = SearchIndex(null, null, null)
-
-    [<Column(IsPrimaryKey=true)>]
-    member this.CustomerId
-        with get() = m_customerId
-        and set(value) = m_customerId <- value
-
+    
+    [<Column(IsPrimaryKey = true)>]
+    member this.Name 
+        with get () = m_name
+        and set (value) = m_name <- value
+    
     [<Column>]
-    member this.CompanyName
-        with get() = m_companyName
-        and set(value) = m_companyName <- value
-
+    member this.Path 
+        with get () = m_path
+        and set (value) = m_path <- value
+    
     [<Column>]
-    member this.ContactName
-        with get() = m_contactName
-        and set(value) = m_contactName <- value
-
-    override this.ToString() = System.String.Format("[{0}] {1}", m_customerId, m_companyName)
+    member this.Type 
+        with get () = m_docType
+        and set (value) = m_docType <- value
+    
+    override this.ToString() = sprintf "Name: '%s' Type: '%s' Path: '%s'" m_name m_docType m_path
 
 let connString = System.String.Format("Data Source={0};UTF8Encoding=True;Version=3", pathToSQLiteDB)
-let conn = new SQLiteConnection(connString);
+let conn = new SQLiteConnection(connString)
 let db = new DataContext(conn)
-let customers = db.GetTable<SearchIndex>()
+let index = db.GetTable<SearchIndex>()
 
 //query for company names
-let companyNameListList = 
-    seq {for i in customers do
-                    yield i.CompanyName}
+let namesList = 
+    seq { 
+        for i in index do
+            yield i.Name
+    }
     |> Seq.toList
-
-
 //find company with id "ALFKI"
 //let alfki =
 //    query <@ seq { for c in customers do
 //                        if c.CustomerId = "ALFKI" then
 //                            yield c}  @>
 //    |> Seq.head
-
 //update name and save to DB
 //alfki.CompanyName <- "Alfreds Futterkiste (test)"
 //db.SubmitChanges()
-
 //run the following code to restore the original name and save it to the DB
 //alfki.CompanyName <- "Alfreds Futterkiste"
 //db.SubmitChanges()
-
 //release resources
 //db.Dispose()
